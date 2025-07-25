d0c3bfc452eda1cc3e57ca97f8a59ff573f7e8dd
karim-99-99 <kareem.hossam99@yahoo.com>
2025-07-25 15:46:14 +0300

    new day new lesson

--- src/companent/Home.jsx
+++ src/companent/Home.jsx
@@ -128,7 +128,7 @@ const Home = () => {
               />
             </a>
             <a
-              href="https://www.linkedin.com/in/karim-khamis-3b6620327/"
+              href="hhttps://www.linkedin.com/in/kareem-khamis-software-engineering6/"
               target="_blank"
               rel="noopener noreferrer"
               className="hover:text-gray-300"
